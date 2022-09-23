4ed646b649b8fe6958db3d8010ab976a76e1d6d9
pasha <pashamedvedev03@gmail.com>
2022-09-24 00:03:01 +0300

    new constants

--- src/main/kotlin/DbConstants.kt
+++ src/main/kotlin/DbConstants.kt
@@ -4,4 +4,7 @@
 object DbConstants {
     const val tablesDirectory = "src\\main\\resources\\tables"
     const val studentsTablePath = "$tablesDirectory\\students.txt"
+    const val variantsTablePath = "$tablesDirectory\\variants.txt"
+    const val testingTablePath = "$tablesDirectory\\testing.txt"
+    var numberOfVariants = 5
 }
