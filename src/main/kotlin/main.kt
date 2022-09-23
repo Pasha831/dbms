ad8cc4f321d4a903f1c40f9d9444b7326153fd98
pasha <pashamedvedev03@gmail.com>
2022-09-24 00:03:24 +0300

    first inflating of new tables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,18 @@
 import data.StudentsTable
+import data.TestingTable
+import data.VariantsTable
 import ui.Gui
 
 fun main() {
     val namesPath = "src\\main\\resources\\names.txt"
 
     val studentsTable = StudentsTable()
+    val variantsTable = VariantsTable()
+    val testingTable = TestingTable()
 
     studentsTable.inflate(namesPath)
-    studentsTable.show()
-
-    println(studentsTable.findStudentById(-7))
+    variantsTable.inflate()
+    testingTable.inflate()
 
     val gui = Gui()
     gui.launchMainScreen()
