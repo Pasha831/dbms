e8be52311b5dd1edc9dd61e9f297b67ee33bf565
pasha <pashamedvedev03@gmail.com>
2022-09-28 21:48:56 +0300

    delete useless code in main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,6 @@
-import data.StudentsTable
-import data.TestingTable
-import data.VariantsTable
 import ui.Gui
 
 fun main() {
-    val namesPath = "tools\\names.txt"
-
-    VariantsTable.inflate()
-    StudentsTable.inflate(namesPath)
-    TestingTable.inflate()
-
     val gui = Gui()
     gui.launchMainScreen()
 }
